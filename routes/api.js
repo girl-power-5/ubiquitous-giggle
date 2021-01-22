df92d566585ffc356b509e05707607d6dbead408
amberchiodini <69092983+amberchiodini@users.noreply.github.com>
2021-01-21 19:00:52 -0600

    added models, passport,& routes to api.js file

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,54 @@
-module.exports = function(app){
-    app.get("/", function(req, res) {
-        res.render("index");
+// Requiring our models and passport 
+var db = require("../models");
+var passport = require("../config/passport");
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
+  
+  app.get("/signup", function (req, res) {
+    res.render("signup");
+  });
+
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
+    res.json(req.user);
+  });
+
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // otherwise send back an error
+  app.post("/api/signup", function (req, res) {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function () {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function (err) {
+        res.status(401).json(err);
       });
-}
+  });
 
+  // Route for logging user out
+  app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 
-
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", function (req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
+  });
+};
